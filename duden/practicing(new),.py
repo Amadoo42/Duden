519cd67e163234780d8e0d8d6212575d56834966
Amadoo42 <68996193+Amadoo42@users.noreply.github.com>
2020-09-09 11:45:16 +0200

    added code def
    functions for every bit of code!

---
+++ duden/practicing(new),.py
@@ -0,0 +1,8 @@
+def ask_about_word():
+    # ask the user about the word
+def ask_about_word_form():
+    # ask about form
+def word_takes_row_id():
+    # row id of word
+def word_number():
+    # the row number of word
