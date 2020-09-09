7b4cfff1580589f1668be62fe027b5d78aa7449e
AhmadAlmotwaly <ahmadmo@yahoo.com>
2020-09-09 12:14:57 +0200

    added parameters

--- duden/practicing(new),.py
+++ duden/practicing(new),.py
@@ -1,8 +1,8 @@
-def ask_about_word():
-    # ask the user about the word
-def ask_about_word_form():
-    # ask about form
+def ask_about_word(row_id):
+    # ask the user about the word 8 forms
+def ask_about_word_form(row_id, word_Form):
+    # ask about one form
 def word_takes_row_id():
-    # row id of word
-def word_number():
-    # the row number of word
+    # row id of word, i dont know what this function do?
+def word_number(word):
+    # the row number of word, i dont know what this function do?
