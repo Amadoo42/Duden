aec2f75dec7a46d93bbffbf9210b424a878d2291
Amadoo42 <68996193+Amadoo42@users.noreply.github.com>
2020-09-09 12:21:45 +0200

    removed last 2 functions as they have no use

--- duden/practicing(new),.py
+++ duden/practicing(new),.py
@@ -2,7 +2,3 @@ def ask_about_word(row_id):
     # ask the user about the word 8 forms
 def ask_about_word_form(row_id, word_Form):
     # ask about one form
-def word_takes_row_id():
-    # row id of word, i dont know what this function do?
-def word_number(word):
-    # the row number of word, i dont know what this function do?
